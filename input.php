869ab5f0b3f2b156e74919a31d6c701e48f826ea
kawase <kawase@zen-platform.jp>
2016-04-06 01:30:36 +0900

    no message

--- input.php
+++ input.php
@@ -2,6 +2,7 @@
 <?php
 echo '【交通費精算システム】一覧<br><br>';
 
+# DBホスト接続
 try {
     $dbh = new PDO('mysql:host=localhost;dbname=dbkeihi;charset=utf8mb4', 'root', 'root');
 } catch (PDOException $e) {
@@ -9,6 +10,7 @@ try {
     die();
 }
 
+# 値セット
 $stmt=$dbh -> prepare("INSERT INTO transport_expenses_details VALUES(?,?,?,?,?,?,?)");
 $stmt->bindParam(':moving_date', $_POST['moving_date'], PDO::PARAM_STR);
 $stmt->bindParam(':origin', $_POST['origin'], PDO::PARAM_STR);
@@ -17,9 +19,7 @@ $stmt->bindParam(':round_trip', $_POST['round_trip'], PDO::PARAM_INT);
 $stmt->bindParam(':cost', $_POST['cost'], PDO::PARAM_INT);
 $stmt->bindParam(':created', date('Y-m-d H:i:s', time()), PDO::PARAM_INT);
 $stmt->bindParam(':modified', date('Y-m-d H:i:s', time()), PDO::PARAM_INT);
-
 $stmt->execute();
-echo $_POST['moving_date']; echo "<BR>"; 
-echo date('Y-m-d H:i:s', time())."<br>";
+
 echo '保存しました。';
 ?>
