e13341e0d80dde08a730f91b6459a0ad57fba0cc
kawase <kawase@zen-platform.jp>
2016-04-06 01:27:19 +0900

    no message

--- input.php
+++ input.php
@@ -1,25 +1,25 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
- "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja" lang="ja">
-<head>
-<meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
-<title>交通費精算システム 明細詳細</title>
-</head>
-<body>
-
-
 
 <?php
+echo '【交通費精算システム】一覧<br><br>';
+
 try {
-    $dbh = new PDO('mysql:host=localhost;dbname=dbkeihi', 'root', 'root');
+    $dbh = new PDO('mysql:host=localhost;dbname=dbkeihi;charset=utf8mb4', 'root', 'root');
 } catch (PDOException $e) {
-    print "Connect Error : ".$dbh->getMessage();
+    echo "Connect Error : ".$dbh->getMessage();
     die();
 }
 
+$stmt=$dbh -> prepare("INSERT INTO transport_expenses_details VALUES(?,?,?,?,?,?,?)");
+$stmt->bindParam(':moving_date', $_POST['moving_date'], PDO::PARAM_STR);
+$stmt->bindParam(':origin', $_POST['origin'], PDO::PARAM_STR);
+$stmt->bindParam(':destination', $_POST['destination'], PDO::PARAM_STR);
+$stmt->bindParam(':round_trip', $_POST['round_trip'], PDO::PARAM_INT);
+$stmt->bindParam(':cost', $_POST['cost'], PDO::PARAM_INT);
+$stmt->bindParam(':created', date('Y-m-d H:i:s', time()), PDO::PARAM_INT);
+$stmt->bindParam(':modified', date('Y-m-d H:i:s', time()), PDO::PARAM_INT);
 
-
-
+$stmt->execute();
+echo $_POST['moving_date']; echo "<BR>"; 
+echo date('Y-m-d H:i:s', time())."<br>";
+echo '保存しました。';
 ?>
-</body>
-</html>
