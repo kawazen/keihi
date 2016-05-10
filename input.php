644c8e8f034f38f6103fcbcb6aa7683236005594
kawase <kawase@zen-platform.jp>
2016-05-10 23:52:35 +0900

    パラメータ指定が間違えていた。以下どちらでもいい模様。 ?,? を指定して $stmt->bindParam(1, ・・・・ :xxxと指定して $stmt->bindParam(":xxx", ・・・・

--- input.php
+++ input.php
@@ -10,17 +10,30 @@ try {
     die();
 }
 
+// # 値セット
+// $stmt=$dbh->prepare("INSERT INTO transport_expenses_details(moving_date,origin,destination,round_trip,cost,created,modified) VALUES (:moving_date,:origin,:destination,:round_trip,:cost,:created,:modified)");
+
+// $stmt->bindParam(':moving_date', $_POST['moving_date'], PDO::PARAM_STR);
+// $stmt->bindParam(':origin', $_POST['origin'], PDO::PARAM_STR);
+// $stmt->bindParam(':destination', $_POST['destination'], PDO::PARAM_STR);
+// $stmt->bindParam(':round_trip', $_POST['round_trip'], PDO::PARAM_INT);
+// $stmt->bindParam(':cost', $_POST['cost'], PDO::PARAM_INT);
+// $now = date('Y-m-d H:i:s', time());
+// $stmt->bindParam(':created', $now, PDO::PARAM_STR);
+// $stmt->bindParam(':modified', $now, PDO::PARAM_STR);
+// $stmt->execute();
+
 # 値セット
 $stmt=$dbh->prepare("INSERT INTO transport_expenses_details(moving_date,origin,destination,round_trip,cost,created,modified) VALUES (?,?,?,?,?,?,?)");
 
-$stmt->bindParam(':moving_date', $_POST['moving_date'], PDO::PARAM_STR);
-$stmt->bindParam(':origin', $_POST['origin'], PDO::PARAM_STR);
-$stmt->bindParam(':destination', $_POST['destination'], PDO::PARAM_STR);
-$stmt->bindParam(':round_trip', $_POST['round_trip'], PDO::PARAM_INT);
-$stmt->bindParam(':cost', $_POST['cost'], PDO::PARAM_INT);
+$stmt->bindParam(1, $_POST['moving_date'], PDO::PARAM_STR);
+$stmt->bindParam(2, $_POST['origin'], PDO::PARAM_STR);
+$stmt->bindParam(3, $_POST['destination'], PDO::PARAM_STR);
+$stmt->bindParam(4, $_POST['round_trip'], PDO::PARAM_INT);
+$stmt->bindParam(5, $_POST['cost'], PDO::PARAM_INT);
 $now = date('Y-m-d H:i:s', time());
-$stmt->bindParam(':created', $now, PDO::PARAM_STR);
-$stmt->bindParam(':modified', $now, PDO::PARAM_STR);
+$stmt->bindParam(6, $now, PDO::PARAM_STR);
+$stmt->bindParam(7, $now, PDO::PARAM_STR);
 $stmt->execute();
 
 echo '保存しました。';
